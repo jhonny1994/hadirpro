1325cd555e78a59ae5b17c66ca5e9f921bccfb93
mohamed abderraouf douadi <me.is.intik@gmail.com>
2025-01-07 10:59:01 +0100

    added locals

--- packages/hadir_ui/lib/features/localization/localization.dart
+++ packages/hadir_ui/lib/features/localization/localization.dart
@@ -1,2 +1,3 @@
 export 'package:hadir_ui/features/localization/application/localization_notifier.dart';
 export 'package:hadir_ui/features/localization/providers/localization_provider.dart';
+export 'package:hadir_ui/features/localization/generated/l10n.dart';
